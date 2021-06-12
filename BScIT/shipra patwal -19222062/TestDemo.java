7b650c8f2217d0dda433b501ae030fcaa480add6
ShipraPatwal <85698677+ShipraPatwal@users.noreply.github.com>
2021-06-12 17:14:41 +0530

    Add files via upload

---
+++ BScIT/shipra patwal -19222062/TestDemo.java
@@ -0,0 +1,17 @@
+class Demo
+{
+ void main()
+{
+ int i;
+for(i=0 ; i<=10 ; i++)
+printf(i)l
+}
+}
+class TestDemo
+{
+public static void main(String[] args)
+{
+  Demo obj = new Demo;
+    obj.Demo();
+}
+}
